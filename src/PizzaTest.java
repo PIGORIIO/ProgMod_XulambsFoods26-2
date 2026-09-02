adcaea299b7cb59799d70c6172c7952edaab0cea
Pedro Gregório <pedrogregoriofernandespontes@gmail.com>
2026-09-02 18:48:40 -0300

    0.1

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -17,4 +17,30 @@ public class PizzaTest {
         assertEquals(4, quantos);
     }
     
+    @Test
+    public void naoadicionaIngredientesNegativos(){
+        //Arrange
+        Pizza pizza = new Pizza();
+
+        //Act
+        int quantos = 
+            pizza.adicionarIngredientes(-5);
+
+        //Assert
+        assertEquals(0, quantos);
+    }
+
+    @Test
+    public void naoadicionaIngredientesEmExcesso(){
+        //Arrange
+        Pizza pizza = new Pizza();
+        int quantos = pizza.adicionarIngredientes(4);
+
+        //Act
+        quantos = 
+            pizza.adicionarIngredientes(7);
+
+        //Assert
+        assertEquals(4, quantos);
+    }
 }
